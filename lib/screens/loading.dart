49db131f02748e08451cc0f46e61386d93130249
ukdong <lklone20005@gmail.com>
2022-01-13 22:51:52 +0900

    feat: Add Loading widget:

---
+++ lib/screens/loading.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class Loading extends StatefulWidget {
+  const Loading({Key? key}) : super(key: key);
+
+  @override
+  _LoadingState createState() => _LoadingState();
+}
+
+class _LoadingState extends State<Loading> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {},
+          child: const Text(
+            "Get my location",
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
